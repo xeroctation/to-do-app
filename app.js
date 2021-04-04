0dfc82a8e647d6dc29e07cd8a4a2c2dff123749d
xeroctation <umidakosimova.uk@gmail.com>
2021-04-05 02:52:08 +0500

    update

--- app.js
+++ app.js
@@ -1,18 +1,75 @@
+
+// third party libs
 const express = require('express')
 const app = express()
+
+// node libs
+const fs = require('fs')
 const PORT = 666
 
 app.set('view engine', 'pug')
-
-app.use('/static', express.static('public'))
+app.use('/static', express.static('assets'))
+app.use(express.urlencoded({ extended: false }))
 
 // localhost:666 
 app.get('/',(req, res) => {
-    res.render('index')
+    fs.readFile('./data/todos.json', (err, data) => {
+        if(err) throw err
+
+        const todos = JSON.parse(data)
+
+        res.render('index', { todos: todos })
+    }) 
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if(formData.todo.trim() == ''){
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todos = JSON.parse(data)
+
+            res.render('index', { error: true, todos: todos })
+        })
+    }   else{
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todos = JSON.parse(data)
+
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false
+            }
+
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/todos.json', (err, data) => {
+                    if (err) throw err
+
+                    const todos = JSON.parse(data)
+
+                    res.render('index', { success: true, todos: todos })
+                })
+            })
+        })
+    }
 })
 
 app.listen(PORT, (err) => {
     if(err) throw err
 
-    console.log('This app is running on port ${ PORT }')
-})
+    console.log(`This app is running on port ${ PORT }`)
+}) 
+
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9)
+  }
